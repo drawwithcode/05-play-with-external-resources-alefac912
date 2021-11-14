2b943feeb4eb060590d10894b45c4499c9ff59e6
alefac <alefac912@users.noreply.github.com>
2021-11-14 17:08:28 +0100

    library update

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@
 // sound
 var song;
 var beat;
-// string
+// strings
+var inst;
 var myText;
 // counters
 var i = -1;
@@ -27,7 +28,8 @@ function preload() {
 
 function setup() {
   // canvas
-  createCanvas(windowWidth, windowHeight);
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.mouseClicked(togglePlay);
   background(0);
   // sound play and amplitude
   song.play();
@@ -37,21 +39,32 @@ function setup() {
   x = width / 3;
   y = height / 4;
   frameRate(10);
-  breakline = [127, 157, 187, 220, 280, 330, 337, 345, 380, 410, 440, 500, 530, 560, 565, 620, 650, 680, 685, 780, 820, 850, 900, 907, 914, 921, 928];
+  breakline = [129, 159, 187, 231, 282, 330, 337, 345, 380, 410, 440, 500, 530, 560, 565, 620, 650, 680, 685, 797, 827, 857, 900, 907, 914, 921, 928];
   // image
   image(leroy, 95, height / 2 - 200, 186, 457);
 
 }
 
 function draw() {
-  // console.log(frameCount);
-  // string of text
+     console.log(frameCount);
+  // strings of text
+  inst = ('To play the song, click two times')
   myText = ('The Typewriter is a short composition of light music by American composer Leroy Anderson, which features an actual typewriter as a percussion instrument. Anderson completed "The Typewriter" in October 9 1950 in Woodbury, Connecticut. "The Typewriter" received its first performance on September 8, 1953 during a recording Anderson and Boston Pops Orchestra made in New York City for Decca Records. Anderson composed melody for symphony and pops orchestras, William Zinn and Floyd Werle arranged it for string orchestras and wind bands respectively. Enjoy!');
+
+  // instructions
+  fill(240);
+  textSize(20);
+  textFont('Special Elite');
+  text(inst, 50, 50);
+
+  // image animation
   // volume mapping
   var volume = beat.getLevel();
   volume = map(volume, 0, 1, 0, 100);
   image(hand, 260 + volume, height / 2, 120, 175);
   image(stick, 77, height / 2 + 125 + volume, 248, 181);
+
+  //text animation
   // ifs cycles
   if (frameCount > 100 && frameCount < 730 || frameCount > 760) {
     if (volume > 4) {
@@ -64,10 +77,20 @@ function draw() {
       j++;
     }
     // console.log(volume);
+    // text function
     var letter = myText.substring(i, i + 1);
-    fill(240);
-    textSize(20);
-    textFont('Special Elite');
     text(letter, x, y);
+  }
+}
+
+// function to play the song (after 2018)
+function togglePlay() {
+  if (song.isPlaying() ){
+    song.pause();
+  } else {
+    song.loop();
+		amplitude = new p5.Amplitude();
+		amplitude.setInput(song);
+    frameCount = 0;
   }
 }
